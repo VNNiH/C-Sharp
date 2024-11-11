64db4ef57c154aed849d19c96d8267890e30f637
Vinicius Benassuli <107008220+VNNiH@users.noreply.github.com>
2024-11-11 18:46:38 -0300

    Update Order.cs

--- C#/Exercicios/Exercicios-Composicao/Exercicio-1/Entities/Order.cs
+++ C#/Exercicios/Exercicios-Composicao/Exercicio-1/Entities/Order.cs
@@ -1,24 +1,57 @@
-public class Order 
+using Course.Entities.Enums;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+
+namespace Course.Entities.Exercicio
 {
-    public DateTime Moment {get; set;}
-    public OrderStatus Status {get; set;}
-    public Client Client {get; set;}
-    public List <OrderItem> Items {get; set;} = new List<OrderItem>();
-    
-    public Order ()
+    class Order
     {
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+        public Client Client { get; set; }
+        List<OrderItem> Items { get; set; } = new List<OrderItem>();
+
+        public Order() { }
+        public Order(DateTime moment, OrderStatus status, Client client)
+        {
+            Moment = moment;
+            Status = status;
+            Client = client;
+        }
+        public void AddItem(OrderItem item)
+        {
+            Items.Add(item);
+        }
+        public void RemoveItem(OrderItem item)
+        {
+            Items.Remove(item);
+        }
+        public double Total()
+        {
+            double sum = 0.0;
+            foreach (OrderItem item in Items)
+            {
+                sum += item.SubTotal();
+            }
+            return sum;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Order moment: " + Moment.ToString("dd/MM/yyyy HH:mm:ss"));
+            sb.AppendLine("Order status: " + Status);
+            sb.AppendLine("Client: " + Client);
+            sb.AppendLine("Order items:");
+            foreach (OrderItem item in Items)
+            {
+                sb.AppendLine(item.ToString());
+            }
+            sb.AppendLine("Total price: $" + Total().ToString("F2", CultureInfo.InvariantCulture));
+            return sb.ToString();
+        }
     }
-    public Order (DateTime moment, OrderStatus status,Client client){
-        Moment = moment;
-        Status = status;
-        Client = client;
-    }
-    
-    public void addItem(OrderItem item){
-        Items.Add(item);
-    }
-    public void removeItem(OrderItem item){
-        Items.Remove(item);
-    }
-    
 }
