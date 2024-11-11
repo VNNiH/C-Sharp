eb9a7aa173ee2cc4bec291c8bc2d99a3948936ac
Vinicius Benassuli <107008220+VNNiH@users.noreply.github.com>
2024-11-11 18:46:29 -0300

    Update Client.cs

--- C#/Exercicios/Exercicios-Composicao/Exercicio-1/Entities/Client.cs
+++ C#/Exercicios/Exercicios-Composicao/Exercicio-1/Entities/Client.cs
@@ -1,17 +1,37 @@
-public class Client
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course.Entities.Exercicio
 {
-    public string Name {get; set;}
-    public string Email{get; set;}
-    public DateTime BirthDate{get; set;}
-    
-    public Client()
+    class Client
     {
-    }
-    
-    public Client(string name, string email, DateTime birthDate)
-    {
-        Name = name;
-        Email = email;
-        BirthDate = birthDate;
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime Birthday { get; set; }
+
+        // Construtor padrão
+        public Client()
+        {
+        }
+
+        // Construtor com todos os atributos
+        public Client(string name, string email, DateTime birthday)
+        {
+            Name = name;
+            Email = email;
+            Birthday = birthday;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Name: {Name}");
+            sb.AppendLine($"Email: {Email}");
+            sb.AppendLine($"Birthday: {Birthday.ToShortDateString()}");
+            return sb.ToString();
+        }
     }
 }
