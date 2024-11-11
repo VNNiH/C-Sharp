4af0b0cf92173956b77f7d107bc9f38affe51de9
Vinicius Benassuli <107008220+VNNiH@users.noreply.github.com>
2024-11-11 18:46:59 -0300

    Update Product.cs

--- C#/Exercicios/Exercicios-Composicao/Exercicio-1/Entities/Product.cs
+++ C#/Exercicios/Exercicios-Composicao/Exercicio-1/Entities/Product.cs
@@ -1,15 +1,28 @@
-public class Product
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course.Entities.Exercicio
 {
-    public string Name {get; set;}
-    public double Price {get; set;}
-    
-    public Product()
+    class Product
     {
-    }
-    
-    public Product(string name, double price)
-    {
-        Name = name;
-        Price = price;
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product() { }
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Name: {Name}");
+            sb.AppendLine($"Price: {Price}");
+            return sb.ToString();
+        }
     }
 }
