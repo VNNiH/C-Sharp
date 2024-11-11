52535ebd4f00dd346b03a33a6595226642079afb
Vinicius Benassuli <107008220+VNNiH@users.noreply.github.com>
2024-11-11 18:46:49 -0300

    Update OrderItem.cs

--- C#/Exercicios/Exercicios-Composicao/Exercicio-1/Entities/OrderItem.cs
+++ C#/Exercicios/Exercicios-Composicao/Exercicio-1/Entities/OrderItem.cs
@@ -1,22 +1,42 @@
-public class OrderItem
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course.Entities.Exercicio
 {
-    public int Quantity {get; set;}
-    public double Price {get; set;}
-    public Product Product{get; set;}
-    
-    public OrderItem()
+    class OrderItem
     {
-    }
-    
-    public OrderItem(int quantity,double price, Product product)
-    {
-        Quantity = quantity;
-        Price = price;
-        Product = product;
-    }
-    
-    public double subTotal()
-    {
-        return Price * Quantity;
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+        public Product Product { get; set; }
+
+        public OrderItem()
+        {
+        }
+        public OrderItem(int quantity, double price, Product product)
+        {
+            Quantity = quantity;
+            Price = price;
+            Product = product;
+        }
+        public double SubTotal()
+        {
+            return Price * Quantity;
+        }
+
+        public override string ToString()
+        {
+            return Product.Name
+                + ", $"
+                + Price.ToString("F2", CultureInfo.InvariantCulture)
+                + ", Quantity: "
+                + Quantity
+                + ", Subtotal: $"
+                + SubTotal().ToString("F2", CultureInfo.InvariantCulture);
+        }
+
     }
 }
