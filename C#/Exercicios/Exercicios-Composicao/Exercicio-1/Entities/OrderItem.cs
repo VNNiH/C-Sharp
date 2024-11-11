68d18a4a8013508622c60f86c076b49b6ee04957
Vinicius Benassuli <107008220+VNNiH@users.noreply.github.com>
2024-11-11 11:27:22 -0300

    Create OrderItem.cs

---
+++ C#/Exercicios/Exercicios-Composicao/Exercicio-1/Entities/OrderItem.cs
@@ -0,0 +1,22 @@
+public class OrderItem
+{
+    public int Quantity {get; set;}
+    public double Price {get; set;}
+    public Product Product{get; set;}
+    
+    public OrderItem()
+    {
+    }
+    
+    public OrderItem(int quantity,double price, Product product)
+    {
+        Quantity = quantity;
+        Price = price;
+        Product = product;
+    }
+    
+    public double subTotal()
+    {
+        return Price * Quantity;
+    }
+}
