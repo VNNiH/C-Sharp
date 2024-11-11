910ee68bf5611dbedec4059168d85f66ef60bf0b
Vinicius Benassuli <107008220+VNNiH@users.noreply.github.com>
2024-11-11 19:30:49 -0300

    Create WorkerLevel.cs

---
+++ C#/Exercicios/Exercicios-Composicao/Exercicio-2/Entities/Enums/WorkerLevel.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course.Entities.Exercicio2.Enums
+{
+    enum WorkerLevel : int
+    {
+        Junior = 0,
+        MidLevel = 1,
+        Senior = 2
+    }
+}
