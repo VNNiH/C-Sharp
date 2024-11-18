8d7319c59ec272340d787b69391a2c54b0f67e62
Vinicius Benassuli <107008220+VNNiH@users.noreply.github.com>
2024-11-18 19:16:02 -0300

    Create BussinesAccount.cs

---
+++ C#/Heranca/Entities/BussinesAccount.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course.Entities.Heranca
+{
+    class BussinesAccount : Account
+    {
+        public double LoanLimit { get; set; }
+
+        public BussinesAccount() { }
+        public BussinesAccount(int number, string holder, double balance, double loanLimit) : base(number,holder,balance)
+        {
+            LoanLimit = loanLimit;
+        }
+
+        public void Loan(double amount)
+        {
+            if(amount <= LoanLimit)
+            {
+                Balance += amount;
+            }
+        }
+    }
+}
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course.Entities.Heranca
+{
+    class BussinesAccount : Account
+    {
+        public double LoanLimit { get; set; }
+
+        public BussinesAccount() { }
+        public BussinesAccount(int number, string holder, double balance, double loanLimit) : base(number,holder,balance)
+        {
+            LoanLimit = loanLimit;
+        }
+
+        public void Loan(double amount)
+        {
+            if(amount <= LoanLimit)
+            {
+                Balance += amount;
+            }
+        }
+    }
+}
