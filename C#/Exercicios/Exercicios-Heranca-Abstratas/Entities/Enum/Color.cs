a4f83a5753d25c9172badf387a6241cb45e2d6d4
Vinicius Benassuli <107008220+VNNiH@users.noreply.github.com>
2024-11-24 15:51:52 -0300

    Create Color.cs

---
+++ C#/Exercicios/Exercicios-Heranca-Abstratas/Entities/Enum/Color.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course.Entities.Enums
+{
+    enum Color
+    {
+        Black,
+        Blue,
+        Red
+    }
+}
