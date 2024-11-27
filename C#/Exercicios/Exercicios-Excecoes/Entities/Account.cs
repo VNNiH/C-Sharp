e0ac5f05fff86922ce2323e2738f4a7032bc7fea
Vinicius Benassuli <107008220+VNNiH@users.noreply.github.com>
2024-11-27 14:22:20 -0300

    Create Account.cs

---
+++ C#/Exercicios/Exercicios-Excecoes/Entities/Account.cs
@@ -0,0 +1,34 @@
+class Account
+{
+	public int Number {get; set;}
+	public string Holder {get; set;}
+	public double Balance {get; set;}
+	public double WithdrawLimit {get; set;}
+
+    public Account(int number, string holder, double balance, double withdrawLimit)
+    {
+        Number = number;
+        Holder = holder;
+        Balance = balance;
+        WithdrawLimit = withdrawLimit;
+    }
+
+	public void Deposit(double amount)
+	{
+		Balance += amount;
+	}
+	public void Withdraw(double amount)
+	{
+		if (amount > WithdrawLimit)
+            {
+                throw new AccountException("The amount exceeds withdraw limit");
+            }
+            if (amount > Balance)
+            {
+                throw new AccountException("Not enough balance");
+            }
+		
+		Balance -= amount;
+	}
+
+}
